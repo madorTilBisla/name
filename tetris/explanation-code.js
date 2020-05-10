a9d4df2a33c1dfd7d9b8588d272eacad1335381e
madorTilBisla <64409705+madorTilBisla@users.noreply.github.com>
2020-05-10 23:00:26 +0300

    Add files via upload

---
+++ tetris/explanation-code.js
@@ -0,0 +1,23 @@
+var elStart1;
+var elStart2
+
+/*
+load
+==================================
+Description: Initialize the program, give explanation of the game and start the game.
+Parameters: none.
+-----------------------------------
+Programmer: rony shoshan.
+Date: 27/03/2020
+-----------------------------------
+*/
+window.addEventListener("load", function() {
+    elStart1 = document.getElementById("game1");
+    elStart2 = document.getElementById("game2");
+    elStart1.addEventListener("click", function() {
+        window.location.href = "main.html"
+    });
+    elStart2.addEventListener("click", function() {
+        window.location.href = "main2.html"
+    });
+});
